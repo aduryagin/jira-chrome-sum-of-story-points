2d01a910f5ad0b117440c854cc06f5d76142d37f
Alexey Duryagin <aiduryagih@gmail.com>
2019-02-13 14:29:52 +0300

    init

---
+++ content.js
@@ -0,0 +1,43 @@
+const columns = document.querySelector('#ghx-pool-column');
+const config = { attributes: true, childList: true, subtree: true };
+
+const callback = function(mutationsList, observer) {
+  document.querySelectorAll('.ghx-column-headers > li.ghx-column').forEach((column, columnIndex) => {    
+    let storyPoints = 0;
+
+    document.querySelectorAll('.ghx-swimlane').forEach(swimlane => {
+      const swimlaneElement = swimlane && swimlane.querySelectorAll('.ghx-column.ui-sortable')[columnIndex];
+      swimlaneElement && swimlaneElement.querySelectorAll('.ghx-extra-field-content').forEach(story => {
+        const time = story.textContent;
+        
+        const timeGroups = /((?<days>[0-9]+)\sdays?)?(, )?((?<hours>[0-9]+)\shours?)?(, )?((?<minutes>[0-9]+)\sminutes?)?/.exec(time).groups;
+      
+        if (timeGroups.days) {
+          storyPoints += Number(timeGroups.days) * 8;
+        }
+        
+        if (timeGroups.hours) {
+          storyPoints += Number(timeGroups.hours);
+        }
+        
+        if (timeGroups.minutes) {
+          storyPoints += Number(timeGroups.minutes) / 60;
+        }
+      });
+    });
+
+    let storyPointsContainer = document.querySelector(`#story-points-container-${columnIndex}`);
+    
+    if (!storyPointsContainer) {
+      storyPointsContainer = document.createElement('div');
+      storyPointsContainer.id = `story-points-container-${columnIndex}`;
+      column.appendChild(storyPointsContainer);
+    }
+
+    storyPointsContainer.textContent = `Story points: ${storyPoints.toFixed(2)}`;
+  });
+};
+
+callback();
+const observer = new MutationObserver(callback);
+observer.observe(columns, config);
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Chrome extension that calculate sum of story points in column for JIRA
+
+<img src="screenshot.png" height="100">
+
+
+## How to use
+
+1) Replace or add your host with jira in manifest.json
+2) [Upload extension to chrome](https://developer.chrome.com/extensions/getstarted) 
