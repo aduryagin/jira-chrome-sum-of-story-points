fe48c9beec47a0d32fd978edd3ba1562acbd35d9
Alexey Duryagin <aiduryagih@gmail.com>
2019-03-15 13:57:02 +0300

    don't count sp's for story

--- content.js
+++ content.js
@@ -7,26 +7,29 @@ const callback = function(mutationsList, observer) {
 
     document.querySelectorAll('.ghx-swimlane').forEach(swimlane => {
       const swimlaneElement = swimlane && swimlane.querySelectorAll('.ghx-column.ui-sortable')[columnIndex];
-      swimlaneElement && swimlaneElement.querySelectorAll('.ghx-extra-field-content').forEach(story => {
-        const time = story.textContent;
-        
-        const timeGroups = /((?<weeks>[0-9]+)\sweeks?)?(, )?((?<days>[0-9]+)\sdays?)?(, )?((?<hours>[0-9]+)\shours?)?(, )?((?<minutes>[0-9]+)\sminutes?)?/.exec(time).groups;
-      
-        if (timeGroups.weeks) {
-          storyPoints += Number(timeGroups.weeks) * 7 * 8;
-        }
 
-        if (timeGroups.days) {
-          storyPoints += Number(timeGroups.days) * 8;
-        }
+      swimlaneElement && swimlaneElement.querySelectorAll('.js-detailview').forEach((task) => {
+        task && !task.querySelector('.ghx-type[title=Story]') && task.querySelectorAll('.ghx-extra-field-content').forEach(timeElement => {
+          const time = timeElement.textContent;
+
+          const timeGroups = /((?<weeks>[0-9]+)\sweeks?)?(, )?((?<days>[0-9]+)\sdays?)?(, )?((?<hours>[0-9]+)\shours?)?(, )?((?<minutes>[0-9]+)\sminutes?)?/.exec(time).groups;
         
-        if (timeGroups.hours) {
-          storyPoints += Number(timeGroups.hours);
-        }
-        
-        if (timeGroups.minutes) {
-          storyPoints += Number(timeGroups.minutes) / 60;
-        }
+          if (timeGroups.weeks) {
+            storyPoints += Number(timeGroups.weeks) * 7 * 8;
+          }
+  
+          if (timeGroups.days) {
+            storyPoints += Number(timeGroups.days) * 8;
+          }
+          
+          if (timeGroups.hours) {
+            storyPoints += Number(timeGroups.hours);
+          }
+          
+          if (timeGroups.minutes) {
+            storyPoints += Number(timeGroups.minutes) / 60;
+          }
+        });
       });
     });
 
