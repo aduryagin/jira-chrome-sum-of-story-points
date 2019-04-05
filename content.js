709a6459aa4e6abc3e134dad822b7267ae7e0a2a
Alexey Duryagin <aiduryagih@gmail.com>
2019-04-05 15:40:49 +0300

    remaining estimate support

--- content.js
+++ content.js
@@ -4,44 +4,82 @@ const config = { attributes: true, childList: true, subtree: true };
 const callback = function(mutationsList, observer) {
   document.querySelectorAll('.ghx-column-headers > li.ghx-column').forEach((column, columnIndex) => {    
     let storyPoints = 0;
+    let remainingStoryPoints = 0;
+
+    const updateSPs = (type, sps) => {
+      if (type === 'original') {
+        storyPoints += sps;
+      }
+
+      if (type === 'remaining') {
+        remainingStoryPoints += sps;
+      }
+    }
+
+    const addSPs = (time, type) => {
+      const timeGroups = /((?<weeks>[0-9]+)\sweeks?)?(, )?((?<days>[0-9]+)\sdays?)?(, )?((?<hours>[0-9]+)\shours?)?(, )?((?<minutes>[0-9]+)\sminutes?)?/.exec(time).groups;
+
+      if (timeGroups.weeks) {
+        const weeks = Number(timeGroups.weeks) * 7 * 8;
+        updateSPs(type, weeks);
+      }
+
+      if (timeGroups.days) {
+        const days = Number(timeGroups.days) * 8;
+        updateSPs(type, days);
+      }
+
+      if (timeGroups.hours) {
+        const hours = Number(timeGroups.hours);
+        updateSPs(type, hours);
+      }
+
+      if (timeGroups.minutes) {
+        const minutes = Number(timeGroups.minutes) / 60;
+        updateSPs(type, minutes);
+      }
+    }
 
     document.querySelectorAll('.ghx-swimlane').forEach(swimlane => {
       const swimlaneElement = swimlane && swimlane.querySelectorAll('.ghx-column.ui-sortable')[columnIndex];
 
       swimlaneElement && swimlaneElement.querySelectorAll('.js-detailview').forEach((task) => {
-        task && !task.querySelector('.ghx-type[title=Story]') && !task.querySelector('.ghx-type[title=Bug]') && task.querySelectorAll('.ghx-extra-field-content').forEach(timeElement => {
-          const time = timeElement.textContent;
+        task &&
+        !task.querySelector('.ghx-type[title=Story]') &&
+        !task.querySelector('.ghx-type[title=Bug]') &&
+        task.querySelectorAll('.ghx-extra-field-row').forEach((rowElement, index) => {
+          const remainingTime = rowElement.querySelector('.ghx-extra-field[data-tooltip^="Σ Remaining Estimate"]');
+          const originalTime = rowElement.querySelector('.ghx-extra-field[data-tooltip^="Σ Original Estimate"]');
 
-          const timeGroups = /((?<weeks>[0-9]+)\sweeks?)?(, )?((?<days>[0-9]+)\sdays?)?(, )?((?<hours>[0-9]+)\shours?)?(, )?((?<minutes>[0-9]+)\sminutes?)?/.exec(time).groups;
-        
-          if (timeGroups.weeks) {
-            storyPoints += Number(timeGroups.weeks) * 7 * 8;
+          if (remainingTime) {
+            addSPs(remainingTime.textContent, 'remaining');
           }
-  
-          if (timeGroups.days) {
-            storyPoints += Number(timeGroups.days) * 8;
-          }
-          
-          if (timeGroups.hours) {
-            storyPoints += Number(timeGroups.hours);
-          }
-          
-          if (timeGroups.minutes) {
-            storyPoints += Number(timeGroups.minutes) / 60;
+
+          if (originalTime) {
+            addSPs(originalTime.textContent, 'original');
           }
         });
       });
     });
 
     let storyPointsContainer = document.querySelector(`#story-points-container-${columnIndex}`);
-    
+    let remainingStoryPointsContainer = document.querySelector(`#remaining-story-points-container-${columnIndex}`);
+
     if (!storyPointsContainer) {
       storyPointsContainer = document.createElement('div');
       storyPointsContainer.id = `story-points-container-${columnIndex}`;
       column.appendChild(storyPointsContainer);
     }
 
+    if (!remainingStoryPointsContainer) {
+      remainingStoryPointsContainer = document.createElement('div');
+      remainingStoryPointsContainer.id = `remaining-story-points-container-${columnIndex}`;
+      column.appendChild(remainingStoryPointsContainer);
+    }
+
     storyPointsContainer.textContent = `Story points: ${storyPoints.toFixed(2)}`;
+    remainingStoryPointsContainer.textContent = `RM story points: ${remainingStoryPoints.toFixed(2)}`;
+
     document.querySelector('#ghx-pool').style.paddingTop = `${document.querySelector('.ghx-column-headers').clientHeight}px`;
   });
 };
